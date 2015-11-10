a91985ffceb38f8b9ee6bf2edbd5cd2436aeafcb
dm007 <houseashley07@hotmail.com>
2015-11-10 19:38:34 +0800

    wave_projection.py 文件中增加批量方波0-1向量聚类映射结果到hist图上的方法，修改wave_test.py中的ones 和one_zeros两个方法为引用Gorilla目录下Exhasut模块中的相同方法

---
+++ core/Wave/wave_projection.py
@@ -0,0 +1,53 @@
+# -*-coding:utf-8 -*-
+# File Name: wave_projection.py
+# Author   : H.Y
+# Date     : 2015-11-10
+
+import numpy as np
+import matplotlib.pyplot as plt
+import wave_test
+
+
+def _getFileName(round, cycles, random, window_size, offset, projecttype):
+    return "pt=" + projecttype +\
+        "_r=" + str(round) +\
+        "_c=" + str(cycles) + \
+        "_rdm=" + bin(random)[2:] + \
+        "_ws="+str(window_size) + \
+        "_offset=" + str(offset)
+
+
+def projection(end_round=1000, cycles=[12, 8, 2], random=False,
+               window_size=10, offset=6, projecttype='p'):
+    """
+    用于生成映射的数据，可选按p和按q映射
+    """
+    xorwave = wave_test.multi_wave_xor(end_round, cycles, random)
+    return wave_test.window_statstic_pjct(window_size, xorwave, offset)
+
+
+def process(end_round=1000, cycles=[12, 8, 2], random=False,
+            window_size=10, offset=6, projecttype='p'):
+    data = projection(end_round, cycles, random,
+                      window_size, offset, projecttype)
+    filename = _getFileName(end_round, cycles, random,
+                            window_size, offset, projecttype)
+    plt.xlabel(projecttype)
+    plt.ylabel('count')
+    plt.hist(data, facecolor='green')
+    plt.savefig(filename)  # 保存文件了~走起
+    plt.close('all')
+
+
+if __name__ == '__main__':
+
+    end_round_set = [100, 500, 1000]
+    cycles_set = [[12, 8, 2], [3, 7, 11]]
+    random_flag = [True, False]
+    window_size_set = [2, 6, 8, 14]
+    offset = 1
+    for end_round in end_round_set:
+        for cycles in cycles_set:
+            for random in random_flag:
+                for window_size in window_size_set:
+                    process(end_round, cycles, random, window_size, offset,'q')
